692abd821b54283ee26aba5a16c2f2c1e89ad19b
kangsb-010313 <kangsb668@naver.com>
2025-05-23 09:13:14 +0900

    sql 250523 09:13

--- Ex/day01.sql
+++ Ex/day01.sql
@@ -88,4 +88,9 @@ select  first_name, last_name,
 from employees;
 
 -- 전체직원의 정보를 다음과 같이 출력하세요
-
+select  concat(first_name, '-', last_name) as 성명,
+		salary as 월급,
+		salary*12 as 연봉,
+		salary*12+5000 as 보너스,
+		phone_number as 전화번호
+from employees;
