677ae12e8b0a64ec5500101df16c9b8c64de1cf7
kangsb-010313 <kangsb668@naver.com>
2025-05-24 19:45:36 +0900

    sql ps01 250524 19:45 push test

--- Personal/Personal01.sql
+++ Personal/Personal01.sql
@@ -5,7 +5,25 @@ from employees
 ;
 
 -- ▪ 도시(locations) 테이블의 모든 내용을 출력해 보세요
+select *
+from locations
+;
+
 -- ▪ 국가(countries) 테이블의 모든 내용을 출력해 보세요
+select *
+from countries
+;
+
 -- ▪ 지역(regions) 테이블의 모든 내용을 출력해 보세요
+
+
 -- ▪ 업무현황(job_history) 테이블의 모든 내용을 출력해 보세요
+
+
 -- ▪ 업무(jobs) 테이블의 모든 내용을 출력해 보세요
+
+
+
+
+
+
