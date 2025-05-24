e314a0667340f6e2fcf7c81fd2a2b8ed5f4c4424
kangsb-010313 <kangsb668@naver.com>
2025-05-24 19:37:42 +0900

    sql ps01 250524 19:37

---
+++ Personal/Personal01.sql
@@ -0,0 +1,11 @@
+use hr_db;
+
+select *
+from employees
+;
+
+-- ▪ 도시(locations) 테이블의 모든 내용을 출력해 보세요
+-- ▪ 국가(countries) 테이블의 모든 내용을 출력해 보세요
+-- ▪ 지역(regions) 테이블의 모든 내용을 출력해 보세요
+-- ▪ 업무현황(job_history) 테이블의 모든 내용을 출력해 보세요
+-- ▪ 업무(jobs) 테이블의 모든 내용을 출력해 보세요
