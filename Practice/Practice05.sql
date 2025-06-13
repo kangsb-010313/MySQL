7034cb21b72d6ce7396de64d3614226cf43c00d9
kangsb-010313 <kangsb668@naver.com>
2025-06-13 23:28:56 +0900

    sql 250613 23:28

--- Practice/Practice05.sql
+++ Practice/Practice05.sql
@@ -67,15 +67,44 @@ order by salary desc
 -출력내용은 매니저아이디, 매니저이름(first_name), 매니저별평균월급, 매니저별최소월급, 매니저별최대월급 입니다.
 (9건)
 */
+select  manager_id
+		,round(avg(salary), 0) avgSalary
+        ,min(salary) minSalary
+        ,max(salary) maxSalary
+from employees
+where hire_date >= '2005-01-01'
+group by manager_id
+having avg(salary) >= 5000
+; 
 
+-- 
+select  m.manager_id
+		,e.first_name
+        ,m.avgSalary
+        ,m.minSalary
+        ,m.maxSalary
+from employees e,(select  manager_id
+						  ,round(avg(salary), 0) avgSalary
+						  ,min(salary) minSalary
+						  ,max(salary) maxSalary
+				  from employees
+				  where hire_date >= '2005-01-01'
+				  group by manager_id
+				  having avg(salary) >= 5000) m
+where e.employee_id = m.manager_id
+;
 
 
 /*
 문제4.
-각 사원(employee)에 대해서 사번(employee_id), 이름(first_name), 부서명(department_name), 매니저(manager)의 이름(first_name)을 조회하세요.
-부서가 없는 직원(Kimberely)도 표시합니다.
+각 사원(employee)에 대해서 사번(employee_id), 이름(first_name),
+부서명(department_name), 매니저(manager)의 이름(first_name)을 조회하세요.
+부서가 없는 직원(Kimberely)은 표시 합니다.
+매니저가 없는 직원(Steven) 은 출력하지 않습니다.
+(106명)
 (106명)
 */
+
 
 
 /*
@@ -83,24 +112,48 @@ order by salary desc
 2005년 이후 입사한 직원중에 입사일이 11번째에서 20번째의 직원의 
 사번, 이름, 부서명, 월급, 입사일을 입사일 순서로 출력하세요
 */
-
+select  employee_id
+		,first_name
+        ,salary
+        ,hire_date
+from employees
+where hire_date >= '2005/01/01'
+limit 10,10
+;
 
 
 /*
 문제6.
 가장 늦게 입사한 직원의 이름(first_name last_name)과 월급(salary)과 근무하는 부서 이름(department_name)은?
 */
+-- 가장 늦은 입사일
+select max(hire_date)
+from employees
+;
 
+-- 합치기 
+select  concat(e.first_name, e.last_name) name
+        ,e.salary
+        ,d.department_name
+        ,e.hire_date
+from employees e, departments d
+where e.department_id = d.department_id
+and e.hire_date = (select max(hire_date)
+				 from employees)
+;
 
 
 /*
 문제7.
 평균월급(salary)이 가장 높은 부서 직원들의 직원번호(employee_id), 이름(firt_name), 성(last_name)과  업무(job_title), 월급(salary)을 조회하시오.
 */
+-- 부서별 평균월급
+select avg(salary)
+from employees
+group by department_id
+;
 
-
-
-
+-- 
 
 /*
 문제8.
