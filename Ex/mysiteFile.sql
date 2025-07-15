c25f704758aaaa054e7bcc3498bc6f8af4a5460c
kangsb-010313 <kangsb668@naver.com>
2025-07-15 14:03:21 +0900

    ms sql filesTable

--- Ex/mysiteFile.sql
+++ Ex/mysiteFile.sql
@@ -8,26 +8,43 @@ show tables;
 
 -- file 조회 
 select  *
-from file
+from files
 ;
 
 -- !!!테이블 삭제!!! --------------------------------------------
-drop table file;
+drop table files;
 
 
 -- 테이블 등록
--- users 테이블
-create table file(
-
+-- file 테이블
+create table files(
+	no			int					primary key		auto_increment
+	,org_name	varchar(200)		not null
+    ,ex_name	varchar(20)			not null
+    ,save_name	varchar(500)		not null
+    ,file_path	varchar(500)	 	not null
+    ,file_size 	bigint				not null
 );
 
 -- ----------------------------------------------
+-- files 전체 조회
+select  no
+		,org_name
+        ,ex_name
+        ,save_name
+        ,file_path
+        ,file_size
+from files
+;
+
 -- 등록 
-insert into users
+insert into files
 values();
 
 -- 삭제 
-delete from users
+delete from files
 where no = ?
 ;
+
+-- 수정 
 
